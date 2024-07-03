7a6f21b03458d8f842ff35f5563e3f866288bfea
Tauhid hossen <tauhidhossen04@gmail.com>
2024-07-03 18:12:30 +0600

    All is done withOut View details button

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,38 +1,27 @@
 import { useEffect, useState } from "react";
-import { Link, useLoaderData, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Slider from "../../Components/Slider/Slider";
 import Feature from "../../Components/Feature/Feature";
 import { SiSymantec } from "react-icons/si";
 import { FaArrowRight, FaGithub, FaSpinner } from "react-icons/fa";
-// import useTitle from "../../hooks/useTitle";
+import useTitle from "../../hooks/useTitle";
 
 const Details = () => {
-  const [data, setData] = useState({})
-
-  const { bookId } = useParams();
-
-  const details = useLoaderData();
+  useTitle('Project Details');
+  const { id } = useParams();
+  const [loader, setLoader] = useState(true);
+  const [data, setData] = useState();
 
   useEffect(() => {
-      const findCart = details?.find((item) => item.bookId === bookId);
+    fetch(`https://rean-portfolio-server.vercel.app/projects/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoader(false);
+      });
+  }, [id]);
 
-      setData(findCart);
-  }, [bookId, details]);
-  // useTitle('Project Details');
-  // const { id } = useParams();
-  // const [loader, setLoader] = useState(true);
-  // const [data, setData] = useState();
-
-  // useEffect(() => {
-  //   fetch(`https://rean-portfolio-server.vercel.app/projects/${id}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setData(data);
-  //       setLoader(false);
-  //     });
-  // }, [id]);
-
-  // console.log(data);
+  console.log(data);
 
   const Technology = ({ f }) => {
     console.log(f);
@@ -52,11 +41,11 @@ const Details = () => {
 
   return (
     <>
-      
+      {loader ? (
         <div className="flex items-center justify-center h-32">
           <FaSpinner className="animate-spin text-white text-4xl" />
         </div>
-      
+      ) : (
         <div>
           <div className="mt-24">
           <Slider data={data}></Slider>
@@ -98,9 +87,118 @@ const Details = () => {
             ))}
           </div>
         </div>
-      
+      )}
     </>
   );
 };
 
 export default Details;
+
+
+// import { useEffect, useState } from "react";
+// import { Link, useLoaderData, useParams } from "react-router-dom";
+// import Slider from "../../Components/Slider/Slider";
+// import Feature from "../../Components/Feature/Feature";
+// import { SiSymantec } from "react-icons/si";
+// import { FaArrowRight, FaGithub, FaSpinner } from "react-icons/fa";
+// import useTitle from "../../hooks/useTitle";
+// // import useTitle from "../../hooks/useTitle";
+
+// const Details = () => {
+//   // const [data, setData] = useState({})
+
+//   // const { bookId } = useParams();
+
+//   // const details = useLoaderData();
+
+//   // useEffect(() => {
+//   //     const findCart = details?.find((item) => item.bookId === bookId);
+
+//   //     setData(findCart);
+//   // }, [bookId, details]);
+//   useTitle('Project Details');
+//   const { id } = useParams();
+//   const [loader, setLoader] = useState(true);
+//   const [data, setData] = useState();
+
+//   useEffect(() => {
+//     fetch(`https://rean-portfolio-server.vercel.app/projects/${id}`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setData(data);
+//         setLoader(false);
+//       });
+//   }, [id]);
+
+//   console.log(data);
+
+//   const Technology = ({ f }) => {
+//     console.log(f);
+//     return (
+//       <div className="flex flex-row p-6 rounded-[20px] feature-card mt-5">
+//         <div className="w-[64px] h-[64px] rounded-full flex items-center justify-center bg-dimBlue">
+//           <SiSymantec size={23} className="text-white"></SiSymantec>
+//         </div>
+//         <div className="flex-1 flex flex-col ml-3">
+//           <p className="font-poppins font-normal text-dimWhite text-[16px] mt-5 leading-[24px]">
+//             {f}
+//           </p>
+//         </div>
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <>
+      
+//         <div className="flex items-center justify-center h-32">
+//           <FaSpinner className="animate-spin text-white text-4xl" />
+//         </div>
+      
+//         <div>
+//           <div className="mt-24">
+//           <Slider data={data}></Slider>
+
+//           </div>
+//           {/* buttons */}
+//           <div className="flex flex-wrap gap-5 md:ml-[480px] mt-10">
+//             <Link to={data?.live_link}>
+//               <button className=" btn btn-sm  bg-purple-100 flex items-center">
+//                 View-Site <FaArrowRight className="ml-1" />
+//               </button>
+//             </Link>
+//             <Link to={data?.client_link}>
+//               <button className=" btn btn-sm bg-purple-100 flex items-center">
+//                 Client Site <FaGithub className="ml-1" size={23} />
+//               </button>
+//             </Link>
+//             <Link to={data?.server_link}>
+//               <button className=" btn btn-sm bg-purple-100 flex items-center">
+//                 Server Site <FaGithub className="ml-1" size={23} />
+//               </button>
+//             </Link>
+//           </div>
+
+//           {/* technology */}
+//           <p className="text-5xl font-bold text-white my-10">Technology Used</p>
+//           <div className="grid grid-cols-2 md:grid-cols-5 gap-5 ml-3">
+//             {data?.technology?.map((f, i) => (
+//               <Technology key={i} f={f}></Technology>
+//             ))}
+//           </div>
+
+//           {/* feature */}
+//           <p className="text-5xl font-bold text-white my-10">Feature</p>
+//           <div>
+//             {data?.feature?.map((f,i ) => (
+              
+//               <Feature key={i} f={f}></Feature>
+//             ))}
+//           </div>
+//         </div>
+      
+//     </>
+//   );
+// };
+
+// export default Details;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -21,8 +21,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/details/:id",
-        element:  <Details></Details>,
-        loader: ()=> fetch('fakr.json')
+        element: <Details></Details>,
         
       },
       {
@@ -55,3 +54,61 @@ const router = createBrowserRouter([
 ]);
 
 export default router;
+
+// import { createBrowserRouter } from "react-router-dom";
+// import Main from "../Layout/Main";
+// import Home from "../Pages/Home/Home/Home/Home";
+// import Details from "../Pages/Details/Details";
+// import AllProject from "../Pages/AllProject/AllProject";
+// import About from "../About/About";
+// import Contact from "../Pages/Contact/Contact";
+// import Project from "../Pages/Home/Home/Project/Project";
+// import MySkills from "../Pages/MySkills/MySkills";
+// import ErrorPage from "./ErrorPage";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Main></Main>,
+//     errorElement: <ErrorPage/>,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home></Home>,
+//       },
+//       {
+//         path: "/details/:id",
+//         element:  <Details></Details>,
+//         loader: ()=> fetch('fakr.json')
+        
+//       },
+//       {
+//         path: "/allProject",
+//         element: <AllProject></AllProject>,
+        
+//       },
+//       {
+//         path: "/about",
+//         element: <About/>,
+        
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact/>,
+        
+//       },
+//       {
+//         path: "/project",
+//         element: <Project/>,
+        
+//       },
+//       {
+//         path: "/skills",
+//         element: <MySkills/> ,
+        
+//       },
+//     ],
+//   },
+// ]);
+
+// export default router;

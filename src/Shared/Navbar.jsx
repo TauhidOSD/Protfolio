524995efde77a7a931bdc2acaf8dc42c600e4a3f
Tauhid hossen <tauhidhossen04@gmail.com>
2024-07-03 16:28:17 +0600

    Resume Added

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
 // import { AiOutlineArrowRight } from "react-icons/ai";
-import resume from "../../src/assets/file/resume/Resume_Rezuan Alam Rean.pdf";
+import resume from "../../src/assets/file/resume/TauhidResume.pdf";
 
 const Navbar = () => {
   const navRoutes = (

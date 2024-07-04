78ec0ec5e01ddee74d29b9030505d2574d63325a
Tauhid hossen <tauhidhossen04@gmail.com>
2024-07-05 00:27:46 +0600

    View details Section responsive

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -23,51 +23,46 @@ const Details = () => {
 
  
   return (
-    
-    <div className="min-h-screen">
-      <section className="bg-gray-800 text-gray-100">
-            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-                   <div>
-                    <img src={data.img1} alt="" />
-                   </div>
-                
-                <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-                    <img src={data.image} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
-                </div>
-                <div className="flex flex-col justify-center p-2 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                    <h1 className="text-2xl font-bold leading-none sm:text-5xl">{data.bookName}
-                        <h1 className="text-xl font-bold leading-none sm:text-xl mt-1">By : {data.author} </h1>
+    <div className="mx-auto lg:flex lg:justify-between max-w-7xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+   <div>
+   <img className="object-cover w-full h-[450px]" src={data.img1} alt="Article" />
+   </div>
 
-                        <h1 className="text-xl font-bold leading-none sm:text-xl mt-1">{data.category}</h1>
+    <div className="px-12 lg:py-24 ">
+       
+          <h2 className="font-bold text-2xl my-4">Description: </h2>
+          <h2 className="font-bold text-2xl ">Features:  </h2>
+          <h2>{data.feature}</h2>
+          <h2 className="font-bold text-2xl  my-4">Technology Used: </h2>
+          <h2>{data.technology}</h2>
 
-                    </h1>
-                    <p className="mt-2  text-lg sm:mb-8"><span className="text-xl font-bold">Review :</span> {data.review}
-
-                    </p>
-                    <div className="flex justify-around ">
-                        <a className="text-xl font-bold">Tag </a>
-
-                        <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline text-[#23BE0A] bg-[#23BE0A0D] p-2 px-4 rounded-xl">{data.bookName}</a>
-                        <a rel="noopener noreferrer" href="#" className="text-xs tracking-wider uppercase hover:underline text-[#23BE0A] bg-[#23BE0A0D] p-2 px-4 rounded-xl">{data.publisher}</a>
-
-                    </div>
-                    <div className="mb-6">
-                        <h1 className="mt-3 "><span className="text-xl font-bold" > Number of Pages :</span>  {data.totalPages}</h1>
-                        <h1 className="mt-3 "><span className="text-xl font-bold" >Publisher:</span> Publisher : {data.publisher}</h1>
-                        <h1 className="mt-3 "><span className="text-xl font-bold" >Year of Publishing :</span>  {data.yearOfPublishing}</h1>
-                        <h1 className="mt-3 "><span className="text-xl font-bold" >Rating : </span> {data.rating}</h1>
-                    </div>
-
-                    <Link to={data.live_link}>
-                    <button>Live</button>
-                    </Link>
-
-
-                  
-                </div>
-            </div>
-        </section>
+         <div className="flex gap-4 my-6">
+         <div>
+          <Link to={data.live_link}><button className="btn btn-sm px-4 bg-purple-200 text-black " >Live Website </button></Link>
+        </div>
+          <div>
+          <Link><button className="btn btn-sm px-4 bg-purple-200 text-black " >Client Code  </button></Link>
+        </div>
+          <div>
+          <Link ><button className="btn btn-sm px-4 bg-purple-200 text-black " >Server Code</button></Link>
+        </div>
+         </div>
+        </div>
+        
     </div>
+
+
+    // <div className="min-h-screen">
+    //   <section className="bg-gray-800 text-gray-100">
+    //         <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+    //                <div>
+    //                 <img src={data.img1} alt="" />
+    //                </div>
+                
+                
+    //         </div>
+    //     </section>
+    // </div>
   );
 };
 
